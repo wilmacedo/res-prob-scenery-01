0107d4812d0f2b32350f83aeeab2657c7d40f91e
wilmacedo <wil.macedo.sa@gmail.com>
2021-09-09 19:03:59 -0300

    add csv to json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Generator = require('objects-to-csv');
+const Parser = require('csvtojson');
 
 const generate = (list, name, filter) => {
   const object = list.filter(item => filter(item));
@@ -9,35 +10,41 @@ const generate = (list, name, filter) => {
 };
 
 try {
-  const rawData = fs.readFileSync(path.join(__dirname, 'Database.json'));
-  const citizins = JSON.parse(rawData);
+  (async () => {
+    const rawData = fs.readFileSync(path.join(__dirname, 'Database.json'));
+    const citizins = JSON.parse(rawData);
 
-  const files = [
-    {
-      name: 'Educação',
-      filter: citizin => citizin.study && !citizin.sick,
-    },
-    {
-      name: 'Saúde',
-      filter: citizin => citizin.sick && !citizins.bus,
-    },
-    {
-      name: 'Mobilidade',
-      filter: citizin => citizin.bus && !citizin.sick,
-    },
-    {
-      name: 'Educação e Saúde',
-      filter: citizin => citizin.study && citizin.sick,
-    },
-    {
-      name: 'Educação e Mobilidade',
-      filter: citizin => citizin.study && citizin.bus,
-    },
-  ];
+    let healthCitizins = await Parser().fromFile(
+      path.join(__dirname, 'Base_Saude.csv'),
+    );
 
-  files.forEach(file => {
-    generate(citizins, file.name, file.filter);
-  });
+    const files = [
+      {
+        name: 'Educação',
+        filter: citizin => citizin.study && !citizin.sick,
+      },
+      {
+        name: 'Saúde',
+        filter: citizin => citizin.sick && !citizins.bus,
+      },
+      {
+        name: 'Mobilidade',
+        filter: citizin => citizin.bus && !citizin.sick,
+      },
+      {
+        name: 'Educação e Saúde',
+        filter: citizin => citizin.study && citizin.sick,
+      },
+      {
+        name: 'Educação e Mobilidade',
+        filter: citizin => citizin.study && citizin.bus,
+      },
+    ];
+
+    files.forEach(file => {
+      generate(citizins, file.name, file.filter);
+    });
+  })();
 } catch (error) {
   console.warn(`Unable to generate CSV. \n${error}`);
 }
